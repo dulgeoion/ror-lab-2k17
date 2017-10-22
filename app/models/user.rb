ff5a5836f55f88caf7a13ab62676899fa68da4c5
dulishkovich\dulishkovich1 <dulgeoion@gmail.com>
2017-10-22 16:29:50 +0300

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-	validates :firstname, :lastname, :email, :country, :adress, :password, presence: true
+	validates :firstname, :lastname, :email, :country, :city, :house, :password, presence: true
 	validates :email, uniqueness: true
 
 end
